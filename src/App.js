8719291334a0a4379f6f54d0cf33a04065d17eb7
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-06-27 03:01:38 +0000

    usestate hook

--- src/App.js
+++ src/App.js
@@ -15,8 +15,15 @@ const MediaCard = ({title,body,imageUrl}) => (
 )
 
 function Room() {
+  const [isLit, setLit] = React.useState(true);
   return (
-    <div className="room">the room is lit</div>
+    <div className="room">
+      the room is {isLit ? 'lit' : 'dark'}
+      <br />
+      <button onClick={() => setLit(!isLit)}>
+        flip
+      </button>
+    </div>
   );
 }
 
